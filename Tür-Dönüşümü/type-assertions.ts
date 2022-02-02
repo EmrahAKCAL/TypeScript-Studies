9097226879761d8e5e226326ea30ee5352a0b5e2
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-02 22:01:36 +0200

    type-assertions

---
+++ Tür-Dönüşümü/type-assertions.ts
@@ -0,0 +1,9 @@
+//Değişken tipi belirterek
+let message ='Hello World';
+let count=message.length; //String bir değişenkenin karakter sayısı
+
+//Değişken tipi belirtmeden
+let mes;
+mes= 'Hello world';
+let counts=(<string>mes).length;
+let Length=(mes as string).length;
